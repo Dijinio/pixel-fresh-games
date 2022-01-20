9c3c1c4aa6cac822d89f6064d2378ccf4078133b
David Jokhadze <djokhadze@gmail.com>
2022-01-20 18:52:03 +0000

    Add files via upload

--- index.js
+++ index.js
@@ -1,5 +1,22 @@
 const screenshots = document.querySelectorAll(".screenshot");
 screenshots.forEach((scr) => scr.addEventListener("click", handleGallery));
+const scrollerButtons = [...document.getElementById("scroller").children];
+const scrollerIcons = [...document.querySelectorAll(".scroll-icon")];
+
+scrollerButtons.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    clearScroller();
+    const firstChild = btn.children[0];
+    const secondChild = firstChild.children[0];
+    secondChild.classList.add("active");
+  });
+});
+
+function clearScroller() {
+  scrollerIcons.forEach((scr) => {
+    scr.classList.remove("active");
+  });
+}
 
 function handleGallery(e) {
   const iconSrc = e.target.src;
@@ -18,3 +35,39 @@ function handleGallery(e) {
     domImage.style.opacity = 0;
   });
 }
+
+const sections = [...document.querySelectorAll(".page-wrapper")];
+
+const appearOnScroll = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      if (!entry.isIntersecting) {
+        return;
+      }
+
+
+      if (entry.target.id === "main") {
+        clearScroller();
+        const scroller = document.getElementById("main-scroller");
+        scroller.classList.add("active");
+      }
+
+      if (entry.target.id === "crypto-stars") {
+        clearScroller();
+        const scroller = document.getElementById("crypto-stars-scroller");
+        scroller.classList.add("active");
+      }
+
+      if (entry.target.id === "car-racer") {
+        clearScroller();
+        const scroller = document.getElementById("car-racer-scroller");
+        scroller.classList.add("active");
+      }
+    });
+  },
+  { threshold: 0.5 }
+);
+
+sections.forEach((section) => {
+  appearOnScroll.observe(section);
+});

0fd0bfb16952b8f073de54669366c75a32c2a0fb
David Jokhadze <djokhadze@gmail.com>
2022-01-23 14:43:34 +0000

    Add files via upload

--- index.js
+++ index.js
@@ -1,16 +1,102 @@
 const screenshots = document.querySelectorAll(".screenshot");
 screenshots.forEach((scr) => scr.addEventListener("click", handleGallery));
+
 const scrollerButtons = [...document.getElementById("scroller").children];
 const scrollerIcons = [...document.querySelectorAll(".scroll-icon")];
+const icons = [...document.querySelector(".main-icons").children];
+const sections = [...document.querySelectorAll(".page-wrapper")];
+let sectionIndex = 0;
+const sectionSize = sections.length;
+
+icons.forEach((icon) => {
+  icon.addEventListener("click", () => {
+    const section = getSection(icon);
+    if (!section) return;
+    activateIcon(section);
+  });
+});
 
 scrollerButtons.forEach((btn) => {
   btn.addEventListener("click", () => {
-    clearScroller();
-    const firstChild = btn.children[0];
-    const secondChild = firstChild.children[0];
-    secondChild.classList.add("active");
+    const section = getSection(btn);
+    activateIcon(section);
   });
 });
+
+function getSection(element) {
+  const href = element.attributes.href.nodeValue;
+  // set global section index
+  sectionIndex = sections.findIndex((s) => s.id === href.replace("#", ""));
+
+  return sections.find((s) => s.id === href.replace("#", ""));
+}
+
+function activateIcon(section) {
+  const iconId = `${section.id}-scroller`;
+  const icon = document.getElementById(iconId);
+  clearScroller();
+  icon.classList.add("active");
+}
+
+let pause = false;
+
+document.addEventListener("wheel", (e) => {
+  if (pause) return;
+  if (e.deltaY < 0) {
+    handlePageChange(false);
+  } else {
+    handlePageChange(true);
+  }
+});
+
+document.addEventListener("touchstart", (e) => {
+  const startPos = e.touches[0].clientY;
+  const startTime = new Date();
+
+  document.addEventListener("touchend", (e) => {
+    if (pause) return;
+
+    const endPos = e.changedTouches[0].clientY;
+    const timeElapsed = new Date() - startTime;
+
+    if (startPos + 100 < endPos && timeElapsed < 1000) {
+      setTimeout(() => {
+        handlePageChange(false);
+      }, 50);
+    }
+    if (startPos - 100 > endPos && timeElapsed < 1000) {
+      setTimeout(() => {
+        handlePageChange(true);
+      }, 50);
+    }
+  });
+});
+
+function handlePageChange(next) {
+  pause = true;
+  const scrollOptions = {
+    block: "start",
+    inline: "nearest",
+    easing: "linear",
+  };
+  const id = manageSection(sectionSize, next);
+  const section = sections[id];
+  activateIcon(section);
+  section.scrollIntoView(scrollOptions);
+  setTimeout(() => {
+    pause = false;
+  }, 700);
+}
+
+function manageSection(size, up) {
+  if (up) {
+    if (sectionIndex >= size - 1) return sectionIndex;
+    return (sectionIndex += 1);
+  } else {
+    if (sectionIndex <= 0) return sectionIndex;
+    return (sectionIndex -= 1);
+  }
+}
 
 function clearScroller() {
   scrollerIcons.forEach((scr) => {
@@ -35,39 +121,3 @@ function handleGallery(e) {
     domImage.style.opacity = 0;
   });
 }
-
-const sections = [...document.querySelectorAll(".page-wrapper")];
-
-const appearOnScroll = new IntersectionObserver(
-  (entries) => {
-    entries.forEach((entry) => {
-      if (!entry.isIntersecting) {
-        return;
-      }
-
-
-      if (entry.target.id === "main") {
-        clearScroller();
-        const scroller = document.getElementById("main-scroller");
-        scroller.classList.add("active");
-      }
-
-      if (entry.target.id === "crypto-stars") {
-        clearScroller();
-        const scroller = document.getElementById("crypto-stars-scroller");
-        scroller.classList.add("active");
-      }
-
-      if (entry.target.id === "car-racer") {
-        clearScroller();
-        const scroller = document.getElementById("car-racer-scroller");
-        scroller.classList.add("active");
-      }
-    });
-  },
-  { threshold: 0.5 }
-);
-
-sections.forEach((section) => {
-  appearOnScroll.observe(section);
-});
